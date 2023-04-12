ac1c31caf492dc52767ab00d8bfebac9f0eac2a3
Sodiums <84286875+Sodiumsss@users.noreply.github.com>
2023-04-12 10:56:31 +0800

    https

--- src/libraries/qwq_function.py
+++ src/libraries/qwq_function.py
@@ -5,73 +5,76 @@ import aiohttp
 
 # 请填写正确的信息。
 devToken = "12345678910"
-url = "localhost"
-port = "6324"
+local = False
+
+url = "localhost" if local else "server.qwq.ski"
+pre = "http://" if local else "https://"
+port = "6325" if local else ""
 
 
 async def verifyQQ(payload):
-    async with aiohttp.request("POST", "http://" + url + ":" + port + "/dev/QQ/verifyQQ",
+    async with aiohttp.request("POST", pre + url + ":" + port + "/dev/QQ/verifyQQ",
                                json=payload, headers={"devToken": devToken}) as resp:
         p = await resp.json()
         return p
 
 
 async def getGamePlayer(payload):
-    async with aiohttp.request("POST", "http://" + url + ":" + port + "/dev/game/getGamePlayer",
+    async with aiohttp.request("POST", pre + url + ":" + port + "/dev/game/getGamePlayer",
                                json=payload, headers={"devToken": devToken}) as resp:
         p = await resp.json()
         return p
 
 async def incGamePlayer(payload):
-    async with aiohttp.request("POST", "http://" + url + ":" + port + "/dev/game/incGamePlayer",
+    async with aiohttp.request("POST", pre + url + ":" + port + "/dev/game/incGamePlayer",
                                json=payload, headers={"devToken": devToken}) as resp:
         p = await resp.json()
         return p
 
 async def incGamePlayers(payload):
-    async with aiohttp.request("POST", "http://" + url + ":" + port + "/dev/game/incGamePlayers",
+    async with aiohttp.request("POST", pre + url + ":" + port + "/dev/game/incGamePlayers",
                                json=payload, headers={"devToken": devToken}) as resp:
         p = await resp.json()
         return p
 async def setGamePlayer(payload):
-    async with aiohttp.request("POST", "http://" + url + ":" + port + "/dev/game/setGamePlayer",
+    async with aiohttp.request("POST", pre + url + ":" + port + "/dev/game/setGamePlayer",
                                json=payload, headers={"devToken": devToken}) as resp:
         p = await resp.json()
         return p
 
 async def decGamePlayer(payload):
-    async with aiohttp.request("POST", "http://" + url + ":" + port + "/dev/game/decGamePlayer",
+    async with aiohttp.request("POST",pre + url + ":" + port + "/dev/game/decGamePlayer",
                                json=payload, headers={"devToken": devToken}) as resp:
         p = await resp.json()
         return p
 async def decGamePlayers(payload):
-    async with aiohttp.request("POST", "http://" + url + ":" + port + "/dev/game/decGamePlayers",
+    async with aiohttp.request("POST", pre + url + ":" + port + "/dev/game/decGamePlayers",
                                json=payload, headers={"devToken": devToken}) as resp:
         p = await resp.json()
         return p
 async def getSongByAlias(payload):
-    async with aiohttp.request("POST", "http://" + url + ":" + port + "/dev/alias/getSongByAlias",
+    async with aiohttp.request("POST", pre + url + ":" + port + "/dev/alias/getSongByAlias",
                                json=payload, headers={"devToken": devToken}) as resp:
         p = await resp.json()
         return p
 
 
 async def getSongAliases(payload):
-    async with aiohttp.request("POST", "http://" + url + ":" + port + "/dev/alias/getSongAlias",
+    async with aiohttp.request("POST", pre + url + ":" + port + "/dev/alias/getSongAlias",
                                json=payload, headers={"devToken": devToken}) as resp:
         p = await resp.json()
         return p['data']
 
 
 async def addSongAlias(payload):
-    async with aiohttp.request("POST", "http://" + url + ":" + port + "/dev/alias/addSongAlias",
+    async with aiohttp.request("POST", pre + url + ":" + port + "/dev/alias/addSongAlias",
                                json=payload, headers={"devToken": devToken}) as resp:
         p = await resp.json()
         return p
 
 
 async def deleteAliasByID(payload):
-    async with aiohttp.request("POST", "http://" + url + ":" + port + "/dev/alias/deleteAliasByID",
+    async with aiohttp.request("POST", pre + url + ":" + port + "/dev/alias/deleteAliasByID",
                                json=payload, headers={"devToken": devToken}) as resp:
         p = await resp.json()
         if p['code'] == 1:
